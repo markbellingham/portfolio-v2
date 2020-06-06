44d7313d1c7db964d4f9ed706368ce03c20d000c
Mark Bellingham <mark.bellingham@protonmail.ch>
2020-06-06 19:58:34 +0100

    Added pure JavaScript function for slideToggle

--- public/common/functions/general.js
+++ public/common/functions/general.js
@@ -48,3 +48,83 @@ export function uuidv4() {
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
+
+export function slideUp(selector, duration) {
+    let target = document.querySelector(selector);
+    /* Slide Up Logic 8 */
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.boxSizing = 'border-box';
+    target.style.height = target.offsetHeight + 'px';
+
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.style.overflow = 'hidden';
+
+    window.setTimeout( () => {
+        target.style.display = 'none';
+        target.style.removeProperty('height');
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+    }, duration);
+
+}
+
+export function slideDown(selector, duration) {
+    let target = document.querySelector(selector);
+    /* Slide Down Logic */
+    target.style.removeProperty('display');
+    let display = window.getComputedStyle(target).display;
+    console.log(display);
+    if(display === 'none') {
+        display = 'block';
+    }
+    target.style.display = display;
+
+    let height = target.offsetHeight;
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.style.overflow = 'hidden';
+    target.offsetHeight;
+
+    target.style.boxSizing = 'border-box';
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+
+    window.setTimeout( () => {
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+    }, duration);
+}
+
+function removeCSSProperties(target, duration) {
+
+
+}
+
+export function slideToggle(selector, duration) {
+    let target = document.querySelector(selector);
+    /* Slide Toggle Logic */
+    if(window.getComputedStyle(target).display === 'none') {
+        return slideDown(selector, duration);
+    } else {
+        return slideUp(selector, duration);
+    }
+}
