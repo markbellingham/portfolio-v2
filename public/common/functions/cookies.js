83f031be004832dfb703f01a667a86dc65f45caf
Mark Bellingham <mark.bellingham@protonmail.ch>
2020-06-29 21:04:34 +0100

    Cookies -  * default age 1 year instead of 1 week  * If cookie is found it is re-written to reset the expiry

--- public/common/functions/cookies.js
+++ public/common/functions/cookies.js
@@ -1,13 +1,27 @@
-export const setCookie = (name, value, days = 7, path = '/') => {
+/**
+ *
+ * @param {string} name - cookie name
+ * @param {string} value - data to be saved to the cookie
+ * @param {number} days - time before expiry
+ * @param {string} path -
+ */
+export const setCookie = (name, value, days = 365, path = '/') => {
     const expires = new Date(Date.now() + days * 864e5).toUTCString()
     document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=' + path
 }
 
 export const getCookie = (name) => {
-    return document.cookie.split('; ').reduce((r, v) => {
+    const cookie = document.cookie.split('; ').reduce((r, v) => {
         const parts = v.split('=')
         return parts[0] === name ? decodeURIComponent(parts[1]) : r
     }, false)
+    if(cookie) {
+        setCookie(name, cookie, 365, '/');
+        console.log('cookie reset');
+        return cookie;
+    } else {
+        return false;
+    }
 }
 
 export const deleteCookie = (name, path) => {
